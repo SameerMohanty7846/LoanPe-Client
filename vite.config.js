bf00d7eb3b018610589c2516ba546d32707e928f
SameerMohanty7846 <sameermohanty6369@gmail.com>
2025-09-22 22:41:02 +0530

    shad cn library added

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,31 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+// import tailwindcss from '@tailwindcss/vite'
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     tailwindcss(),
+//     react()
+
+
+
+//   ],
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path' // 👈 add this
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     tailwindcss(),
     react()
-
-
-
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src') // 👈 this sets up @/ as alias for src/
+    }
+  }
 })
